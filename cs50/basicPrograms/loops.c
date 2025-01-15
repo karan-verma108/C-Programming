f04e58c11b60dea8e61b5b3bb8c70dc115fd21bb
karan <karanverma201411@gmail.com>
2025-01-15 06:49:16 +0530

    C : for and while loop

---
+++ cs50/basicPrograms/loops.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* while loop
+    int counter = 3;
+    while (counter > 0)
+    {
+        printf("meow\n");
+        counter--;
+    } */
+
+    //    for loop
+    for (int counter = 0; counter < 3; counter++)
+    {
+        printf("meow\n");
+    }
+}
